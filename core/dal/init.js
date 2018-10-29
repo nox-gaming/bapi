0ba151a30f082f4e675bfcc5975801659986e5a0
Ugo Arzur <ugo.arzur@gmail.com>
2018-10-29 18:57:25 +0100

    use the .env files for futur credentials

--- core/dal/init.js
+++ core/dal/init.js
@@ -3,9 +3,9 @@ const knex = require('knex')({
     client: 'pg',
     connection: {
         host: 'localhost',
-        user: 'postgres',
-        password: 'password',
-        database: 'nox_guild'
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
     }
 });
 
