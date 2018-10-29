6815f23a664e9130ecab0df763c7a77ad17ed5e1
Ugo Arzur <ugo.arzur@gmail.com>
2018-10-29 18:58:38 +0100

    fetch from db data, if error log it

--- core/routes/api/plan.js
+++ core/routes/api/plan.js
@@ -1,14 +1,14 @@
 const express = require('express')
+const signale = require('signale')
 const router = express.Router()
-const db = require('../../dal/index')
 
+const db = require('../../dal/index')
 
 router.get('/', function (req, res) {
     db.select().from('plans').then(data => {
-        console.log(data);
-        
+        signale.success(data);
         res.send(data)
-    });
+    }).catch(e => signale.error('Oops', e))
 })
 
 module.exports = router;
