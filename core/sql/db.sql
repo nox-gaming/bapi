1d75e95c2bc437d2086cc1a26ca939d12e0bdd34
Ugo Arzur <ugo.arzur@gmail.com>
2019-01-06 10:19:37 +0100

    update db structure

--- core/sql/db.sql
+++ core/sql/db.sql
@@ -2,6 +2,8 @@ CREATE DATABASE "nox_guild" WITH TEMPLATE = template0;
 
 \connect "nox_guild";
 
+DROP TABLE IF EXISTS requests, schedules, events, teams, members, games CASCADE;
+
 -- FUNCTION next_id to process automated id
 CREATE FUNCTION next_id (OUT result bigint)
   RETURNS bigint
@@ -33,7 +35,7 @@ CREATE SEQUENCE table_id_seq
 
 -- REQUESTS GATEWAY
 CREATE TABLE requests (
-  id bigint NOT NULL DEFAULT next_id(),
+  id bigint NOT NULL DEFAULT next_id() PRIMARY KEY,
   request_id text,
   ip text,
   user_agent text,
@@ -44,11 +46,23 @@ CREATE TABLE requests (
   created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
+-- GAMES
+create table "games" (
+  id serial NOT NULL PRIMARY KEY,
+  hash text,
+  title text,
+  type text,
+  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+  UNIQUE(hash)
+);
+
 -- SCHEDULE
 create table "schedules" (
-  id serial NOT NULL,
+  id serial NOT NULL PRIMARY KEY,
   title text,
   is_finished boolean,
+  organizers text,
+  team text,
   expiration_date text,
   created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
@@ -56,16 +70,41 @@ create table "schedules" (
 -- EVENTS BELONGING TO SCHEDULE
 create table "events" (
   id serial NOT NULL,
-  schedule_id integer NOT NULL,
+  schedule_id integer REFERENCES schedules(id),
   title text,
   is_finished boolean,
-  response text,
+  description text,
   expiration_date text,
+  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id, schedule_id)
+);
+
+-- TEAMS
+create table "teams" (
+  id serial NOT NULL PRIMARY KEY,
+  hash text,
+  title text,
+  tag text,
+  game_id integer REFERENCES games(id) NOT NULL,
+  is_dissolved boolean,
+  squads text,
   created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
-ALTER TABLE ONLY "schedules" ADD CONSTRAINT schedule_pk PRIMARY KEY (id);
-ALTER TABLE ONLY "events" ADD CONSTRAINT events_pk PRIMARY KEY (id);
+-- MEMBERS
+create table "members" (
+  id serial NOT NULL PRIMARY KEY,
+  firstname text,
+  lastname text,
+  nickname text,
+  squads text,
+  teams text,
+  created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
 
 -- INSERT DATA
-insert into "schedules" (title, is_finished) values ('first schedule', false);
+insert into "members" (firstname, lastname, nickname, squads, teams) values ('Ugo', 'Arzur', 'Carbo', '["Blackwatch"]', '["Blackwatch"]');
+insert into "games" (hash, title, type) values ('abcd', 'Overwatch', '["fps"]');
+insert into "teams" (hash, title, is_dissolved, tag, squads, game_id) values ('xb03hi','Overwatch team', false, 'lgrf', '["Overwatch"]', 1);
+insert into "schedules" (title, is_finished, team) values ('Overwatch Schedule', false, 'xb03hi');
+insert into "events" (schedule_id, title, is_finished, description, expiration_date) values (1, 'Overwatch Schedule', false, 'hallo','xb03hi');
